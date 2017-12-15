477520bf02bcfbd1de6f8386a1a4ca43771d15ea
Hakan Kimeiga <hak7alp@gmail.com>
2017-12-14 20:14:04 -0800

    init1

---
+++ cmake-build-debug/CMakeFiles/project7warmup.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project7warmup.dir/main.cpp.o"
+  "project7warmup.pdb"
+  "project7warmup.exe"
+  "libproject7warmup.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project7warmup.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
